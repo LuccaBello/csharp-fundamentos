0ba2663040d3baa3d053fbff4db8b5a8105bb56d
unknown <bellolucca@gmail.com>
2026-08-18 20:42:44 -0300

    Add while structure exercises

---
+++ estrutura-while/Exercicio2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] coordenadas = Console.ReadLine().Split(' ');
+            int x = int.Parse(coordenadas[0]);
+            int y = int.Parse(coordenadas[1]);
+
+            while (x != 0 && y != 0)
+            {
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("primeiro");
+                }
+
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("segundo");
+                }
+
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("terceiro");
+                }
+
+                else if (x > 0 && y < 0)
+                {
+                    Console.WriteLine("quarto");
+                }
+
+                coordenadas = Console.ReadLine().Split(' ');
+                x = int.Parse(coordenadas[0]);
+                y = int.Parse(coordenadas[1]);
+            }
+        }
+    }
+}
