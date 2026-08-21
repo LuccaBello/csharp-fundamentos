37b565cb143eb5d24700f48c31afc0af69e9b3f1
unknown <bellolucca@gmail.com>
2026-08-21 03:09:34 -0300

    Add classes exercises 3-5

---
+++ classes-objetos/Exercicio5/Aluno.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Exercicio5
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double NotaTotal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+    }
+}
---
+++ classes-objetos/Exercicio5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = " + aluno.NotaTotal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (aluno.NotaTotal() >= 60)
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                double restante = 60 - aluno.NotaTotal();
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + restante.ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+        }
+    }
+}
