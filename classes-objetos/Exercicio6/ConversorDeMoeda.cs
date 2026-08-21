429ca30fcd989aed53741272fb158ac98b9b86ed
unknown <bellolucca@gmail.com>
2026-08-21 20:01:41 -0300

    Add static members exercise

---
+++ classes-objetos/Exercicio6/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercicio6
+{
+    class ConversorDeMoeda
+    {
+        public static double IOF = 1.06;
+
+        public static double Total (double dolar, double cotacao)
+        {
+            return dolar * cotacao * IOF;
+        }
+    }
+}
---
+++ classes-objetos/Exercicio6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais = "
+                + ConversorDeMoeda.Total(dolares, cotacao).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
