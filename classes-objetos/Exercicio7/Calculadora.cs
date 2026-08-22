56b9e99781f93cc9914e1c13e81363fc8017753f
unknown <bellolucca@gmail.com>
2026-08-21 21:17:22 -0300

    Add static members exercise

---
+++ classes-objetos/Exercicio7/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio7
+{
+    class Calculadora
+    {
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double raio)
+        {
+            return 2.0 * Pi * raio;
+        }
+
+        public static double Volume(double raio)
+        {
+            return 4.0 / 3.0 * Pi * raio * raio * raio;
+        }
+    }
+}
---
+++ classes-objetos/Exercicio7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o valor do raio: ");
+            double r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Circunferência: " 
+                + Calculadora.Circunferencia(r).ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Volume: " 
+                + Calculadora.Volume(r).ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
